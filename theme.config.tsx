ce8a74494334ab628ccce977d6389e2e04234472
Doug Silkstone <doug@withseismic.com>
2023-06-08 20:32:14 +0200

    edit/github-links

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>Practical Guides From A Self-Taught Technical Lead</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/dougwithseismic/career-bible-2023",
   },
   chat: {
     link: "https://twitter.com/dougiesilkstone",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/dougwithseismic/career-bible-2023",
   footer: {
     text: "A growing compendium of my career knowledge. Updated Weekly.",
   },
